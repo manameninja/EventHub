da79244bf4a6f470fc1b9be25e83d7ab4535f2f0
nikita <nikmosyl@gmail.com>
2024-11-24 16:04:51 +0400

    add search view

---
+++ EventHub/Screens/Search/SearchView.swift
@@ -0,0 +1,12 @@
+//
+//  SearchView.swift
+//  EventHub
+//
+//  Created by nikita on 24.11.24.
+//
+
+import UIKit
+
+final class SearchView: UIView {
+    
+}
--- EventHub/Screens/Search/SearchViewController.swift
+++ EventHub/Screens/Search/SearchViewController.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
+final class SearchViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
-        // Do any additional setup after loading the view.
     }
 
 }
