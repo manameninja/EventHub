48913f7287d94e8b1733ea15aa04c544951b9380
nikita <nikmosyl@gmail.com>
2024-11-24 18:48:41 +0400

    add layout

--- EventHub/Screens/Search/SearchView.swift
+++ EventHub/Screens/Search/SearchView.swift
@@ -7,6 +7,112 @@
 
 import UIKit
 
+protocol SearchViewProtocol: AnyObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func didTappedFilterButton()
+}
+
 final class SearchView: UIView {
+    weak var delegate: SearchViewProtocol?
     
+    private let searchField: UISearchTextField = {
+        let field = UISearchTextField()
+        return field
+    }()
+    
+    private let filterhButton: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.image = UIImage(systemName: "magnifyingglass")
+        config.baseBackgroundColor = .clear
+        config.baseForegroundColor = .label
+        let button = UIButton(configuration: config)
+        return button
+    }()
+    
+    private let noFavoritesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "NO RESULTS"
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .label
+        return label
+    }()
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.register(EventCell.self, forCellWithReuseIdentifier: EventCell.description())
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .backgroundLightGray
+        
+        setupUI()
+        setupConstrainst()
+        setupTargets()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupDelegates(_ vc: SearchViewProtocol) {
+        collectionView.delegate = vc
+        collectionView.dataSource = vc
+        delegate = vc
+    }
+    
+    func hideNoData(_ state: Bool) {
+        noFavoritesLabel.isHidden = state
+    }
+    
+    @objc func buttonTapped() {
+        delegate?.didTappedFilterButton()
+    }
+}
+
+//MARK: - Setup UI
+private extension SearchView {
+    func setupUI() {
+        backgroundColor = .backgroundLightGray
+        [
+            searchField,
+            filterhButton,
+            noFavoritesLabel,
+            collectionView
+        ].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            addSubview($0)
+        }
+    }
+    
+    func setupConstrainst() {
+        NSLayoutConstraint.activate([
+            filterhButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 32),
+            filterhButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -24),
+            filterhButton.heightAnchor.constraint(equalToConstant: 32),
+            filterhButton.widthAnchor.constraint(equalToConstant: 75),
+            
+            searchField.topAnchor.constraint(equalTo: filterhButton.topAnchor),
+            searchField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24),
+            searchField.trailingAnchor.constraint(equalTo: filterhButton.leadingAnchor, constant: -8),
+            searchField.bottomAnchor.constraint(equalTo: filterhButton.bottomAnchor),
+            
+            noFavoritesLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            noFavoritesLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: filterhButton.bottomAnchor, constant: 32),
+            collectionView.leadingAnchor.constraint(equalTo: filterhButton.leftAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: filterhButton.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    func setupTargets() {
+        filterhButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
 }
--- EventHub/Screens/Search/SearchViewController.swift
+++ EventHub/Screens/Search/SearchViewController.swift
@@ -8,10 +8,69 @@
 import UIKit
 
 final class SearchViewController: UIViewController {
+    private let searchView = SearchView()
     
+    private var inputEventList: [Event]
+    private var filteredEventList: [Event] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view = searchView
+        searchView.setupDelegates(self)
+        if !inputEventList.isEmpty { searchView.hideNoData(true) }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        eventList = StorageManager.shared.loadFavorite()
+    }
 }
+
+//MARK: - SearchViewProtocol
+extension SearchViewController: SearchViewProtocol {
+    func didTappedFilterButton() {
+        print(#function)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        filteredEventList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: EventCell.description(),
+            for: indexPath
+        ) as? EventCell else { return UICollectionViewCell() }
+        
+        let event = filteredEventList[indexPath.item]
+        let favorite = StorageManager.shared.loadFavorite().contains { $0.id == event.id }
+        
+        cell.configure(
+            imageURL: URL(string: event.images?.first?.imageUrl ?? ""),
+            isFavorite: favorite,
+            date: event.nextDate,
+            title: event.title ?? "unknown",
+            address: event.place?.address ?? event.place?.title ?? "unknown"
+        )
+        cell.makeFavorite(true)
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 106)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\nбыла нажата ячейка:", indexPath, "\n")
+        #warning("TO DO: вставить переход на детеил")
+    }
+}
+
+//MARK: - EventCellDelegate
+extension SearchViewController: EventCellDelegate {
+    func didTapButton(in cell: EventCell) {
+        
+    }
+}
+
