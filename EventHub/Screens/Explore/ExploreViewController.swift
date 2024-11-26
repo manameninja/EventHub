7e9db5240edf5699998dbfe3ca7c280dfebe7912
Daniil Pavlenko <danyy.pavl@gmail.com>
2024-11-26 23:03:59 +0300

    remove tab on empty rows

--- EventHub/Screens/Explore/ExploreViewController.swift
+++ EventHub/Screens/Explore/ExploreViewController.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
-    
+
 }
