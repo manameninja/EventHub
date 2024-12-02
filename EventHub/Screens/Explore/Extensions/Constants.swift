bf40e5b782f4816b4f58e2abfa28e49ebc46b0fc
Bokhulenkov <bokhulenkov@gmail.com>
2024-12-02 16:17:08 +0300

    feat [bokhulenkov] add images to category

--- EventHub/Screens/Explore/Extensions/Constants.swift
+++ EventHub/Screens/Explore/Extensions/Constants.swift
@@ -8,9 +8,66 @@
 import UIKit
 
 enum CategoryImage {
-    static let food = UIImage(named: "food")
-    static let game = UIImage(named: "game")
-    static let music = UIImage(named: "music")
+    case cinema
+    case concert
+    case education
+    case entertainment
+    case exhibition
+    case fashion
+    case festival
+    case holiday
+    case kids
+    case other
+    case party
+    case photo
+    case quest
+    case recreation
+    case shopping
+    case stock
+    case theater
+    case tour
+
+    var image: UIImage? {
+        switch self {
+        case .cinema:
+            return UIImage(systemName: "film.circle")
+        case .concert:
+            return UIImage(systemName: "theatermasks.circle")
+        case .education:
+            return UIImage(systemName: "book.circle")
+        case .entertainment:
+            return UIImage(systemName: "party.popper")
+        case .exhibition:
+            return UIImage(systemName: "building.columns.circle")
+        case .fashion:
+            return UIImage(systemName: "eyebrow")
+        case .festival:
+            return UIImage(systemName: "flag.2.crossed.circle")
+        case .holiday:
+            return UIImage(systemName: "globe.americas.fill")
+        case .kids:
+            return UIImage(systemName: "figure.walk.motion")
+        case .other:
+            return UIImage(systemName: "accessibility")
+        case .party:
+            return UIImage(systemName: "party.popper")
+        case .photo:
+            return UIImage(systemName: "camera.circle")
+        case .quest:
+            return UIImage(systemName: "stopwatch")
+        case .recreation:
+            return UIImage(systemName: "sun.max.circle.fill")
+        case .shopping:
+            return UIImage(systemName: "cart")
+        case .stock:
+            return UIImage(systemName: "camera.metering.unknown")
+        case .theater:
+            return UIImage(systemName: "theatermasks.circle")
+        case .tour:
+            return UIImage(systemName: "storefront")
+        }
+    }
+    
 }
 
 enum K {
